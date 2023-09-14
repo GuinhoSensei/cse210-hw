29b289c3f1335b62fd6cb7833c0e05bb285547cc
GuinhoSensei <thiggouveia@hotmail.com>
2023-09-14 10:22:43 -0700

    Prep 2 Finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade percentage");
+        string GradePercet = Console.ReadLine();
+        int grade = int.Parse(GradePercet);
+        string letter = "";
+
+        if (grade >= 90){
+            letter = "A";
+        }
+        else if (grade >= 80){
+            letter = "B";
+        }
+        else if (grade >= 70){
+            letter = "C";
+        }
+        else if (grade >= 60){
+            letter = "D";
+        }
+        else if (grade < 60){
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+        
     }
 }
