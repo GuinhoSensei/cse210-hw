18981f6494f640fb73b10b24484712e3b93d4b36
GuinhoSensei <thiggouveia@hotmail.com>
2023-09-21 19:46:47 -0700

    practice 3 done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Random randomGenerator = new Random();
+        int Number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != Number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (Number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (Number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
